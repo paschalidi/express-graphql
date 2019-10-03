2dcdbd407875d48f9a1edc2d8d96bd83bfda5a53
Christos Paschalidis <paschalidi@outlook.com>
2019-10-03 20:23:20 +0200

    index ts

--- src/index.ts
+++ src/index.ts
@@ -4,30 +4,30 @@ import Express from "express";
 import { buildSchema } from "type-graphql";
 import { createConnection } from "typeorm";
 import session from "express-session";
-import connectRedis from "connect-redis";
 import cors from "cors";
-import Redis from "ioredis";
-import { MeResolver } from "./modules/user/Me";
-import { RegisterResolver } from "./modules/user/Register";
-import { LoginResolver } from "./modules/user/Login";
-
-const redis = new Redis();
+import { redis } from "./redis";
+import connectRedis from "connect-redis";
 
 const main = async () => {
+  // init typeorm
   await createConnection();
+
+  // init type-graphql
   const schema = await buildSchema({
-    resolvers: [RegisterResolver, LoginResolver, MeResolver]
+    resolvers: [__dirname + "/modules/**/*.ts"],
+    authChecker: ({ context: { req } }) => !!req.session.userId
   });
+
+  //init apollo server
   const apolloServer = new ApolloServer({
     schema,
     // pass a function to the context key. Apollo server gives us access to the context key.
     // we can access this object req into our resolvers
-    context: ({ req }: any) => ({ req })
+    context: ({ req, res }: any) => ({ req, res })
   });
-  const app = Express();
 
+  // init redis connection
   const RedisStore = connectRedis(session);
-
   const sessionOption: session.SessionOptions = {
     store: new RedisStore({
       client: redis as any
@@ -42,6 +42,9 @@ const main = async () => {
       maxAge: 1000 * 60 * 60 * 24 * 7 * 365 // 7 years
     }
   };
+
+  // init express server
+  const app = Express();
 
   app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
   app.use(session(sessionOption));
